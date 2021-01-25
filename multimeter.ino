b5f425d41147db393c286e3e0a3936ccb350ecd9
Siddharth Reddy <sidd_tim@outlook.com>
2021-01-25 15:38:10 +0530

    Update multimeter.ino

--- multimeter.ino
+++ multimeter.ino
@@ -4,13 +4,11 @@ float v;
 int sleep = 750;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(pinNo,INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   readOutput = analogRead(pinNo);
   v = (5. / 1023.) * readOutput;
   Serial.println(v);
